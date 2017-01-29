7fb0b3577b25a9cc932ea8085d808a176da43ba5
eral.r.l <eral.r.l+github.com@gmail.com>
2017-01-30 03:23:22 +0900

    :pencil:Write license
    ライセンス記述

--- Assets/EdgeAnimationDesignerAssistance/Scripts/Editor/EdgeAnimation.cs
+++ Assets/EdgeAnimationDesignerAssistance/Scripts/Editor/EdgeAnimation.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// (C) 2016 ERAL
+// Distributed under the Boost Software License, Version 1.0.
+// (See copy at http://www.boost.org/LICENSE_1_0.txt)
+
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
